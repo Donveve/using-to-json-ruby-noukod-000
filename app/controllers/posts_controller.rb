af79c40862d4f56937ed4c4c553e74d11d1c3423
Veniel Dorestave <veniel@noukod.com>
2019-03-15 19:34:38 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,12 +6,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
-    respond_to do |format|
-      format.html { render :show }
-      format.json { render json: @post.to_json(only: [:title, :description, :id],
-                              include: [author: { only: [:name]}]) }
-    end
+    @post = Post.find_by(id:params[:id])
   end
 
   def new
@@ -34,10 +29,9 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    #render json: PostSerializer.serialize(post)
-    render json: post.to_json(only: [:title, :description, :id],
-                              include: [ author: { only: [:name]}])
+    render json: post.t_json
   end
+
 private
   # Use callbacks to share common setup or constraints between actions.
   def set_post
